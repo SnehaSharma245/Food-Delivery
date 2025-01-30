fc93c2c0816fc42aba928b085369f24719fa123f
Sneha Sharma <snehav2109@gmail.com>
2025-01-30 14:59:32 +0530

    changes_2

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      " mongodb+srv://snehav2109:Snehas_874@cluster0.v6b7c.mongodb.net/snehaDB"
+      "mongodb+srv://snehav2109:Snehas_874@cluster0.v6b7c.mongodb.net/snehaDB"
     )
     .then(() => console.log("DB Connected"));
 };
